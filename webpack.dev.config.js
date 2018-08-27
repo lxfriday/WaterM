5ab57cf26a5bdadebbff3fdbb9c3072735d77780
lxfriday <liu3248184446@outlook.com>
2018-08-27 22:16:20 +0800

    [modify] 在dev模式下添加HMR功能

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,6 +4,7 @@
  * @author lxfriday
  */
 const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
 const webpackMerge = require('webpack-merge');
 
 const webpackBaseConfig = require('./webpack.base.config');
@@ -11,11 +12,15 @@ const webpackBaseConfig = require('./webpack.base.config');
 module.exports = webpackMerge(webpackBaseConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [
+    new HotModuleReplacementPlugin(),
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     compress: false,
     progress: true,
     port: 9000,
     inline: true,
+    hot: true,
   },
 });
