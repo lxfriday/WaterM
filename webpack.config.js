b13e7ba07d741bce7f21fac2bf1444b54f2a7582
lxfriday <liu3248184446@outlook.com>
2018-08-27 12:43:02 +0800

    [modify] 修改编译配置

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   module: {
     rules: [
       {
