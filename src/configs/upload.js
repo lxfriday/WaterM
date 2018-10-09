3b1ae1a6b1016938952e2cae978194a0631adaf0
lxfriday <liu3248184446@outlook.com>
2018-10-09 09:06:38 +0800

    [modify] build 之后使用的协议是 file，修改所有协议为绝对协议

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -6,9 +6,9 @@
 
 export default {
   // 上传时候的目标网址
-  action: '//upload.qiniup.com',
+  action: 'https://upload.qiniup.com',
   // 显示图片的时候的自己的域名
-  domain: '//qiniu1.lxfriday.xyz/',
+  domain: 'http://qiniu1.lxfriday.xyz/',
   ak: 'zlvMwqEQx1Yir9urPO0cwCAUt-7PMXUJWZnRY35S',
   sk: 'ya4exb9R3oCkAJiABsel5KiiUBKLZebvP2KyG289',
   bucket: 'storage',
